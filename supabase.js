2b2dcf637f7434cbd571a96593ed4eb417115c4d
carlonikola <81236411+carlonikola@users.noreply.github.com>
2026-03-09 23:39:16 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // ═══ SUPABASE CONFIG ═══
 // HIER DEINE DATEN EINFÜGEN:
 const SUPABASE_URL = 'https://teqoajmnqrdantvrbcgs.supabase.co';
-const SUPABASE_KEY = 'DEIN_API_KEY_HIER_EINFUEGEN'; // ← Project Settings → API → anon/public
+const SUPABASE_KEY = 'sb_publishable__rl4Ft5kUTrS2dTNFsg2Lw_qpuIPvMY'; // ← Project Settings → API → anon/public
 
 // Supabase CDN laden
 const script = document.createElement('script');
